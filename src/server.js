6f67c29a340227277babcc262b831a4146620b08
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-12-03 16:23:46 +0200

    login

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -19,6 +20,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
