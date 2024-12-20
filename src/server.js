60241ab7a805c5d339d6137c3cd4a662c855660c
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-12-20 23:09:31 +0200

    finish

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use('/api-docs', swaggerDocs());
   app.use(cookieParser());
   app.use(
     pino({
@@ -44,6 +45,8 @@ export const setupServer = () => {
 
   app.use(router);
 
+  app.use('/auth/uploads', express.static(UPLOAD_DIR));
+
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
@@ -51,8 +54,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  app.use('/auth/uploads', express.static(UPLOAD_DIR));
-
-  app.use('/api-docs', swaggerDocs());
 };
